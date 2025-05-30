3a3906b974db6d96f71e5cc48be36d803ed5584d
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2025-05-30 14:48:53 +0200

    style: Better error pages

--- apps/web/app/local/page.tsx
+++ apps/web/app/local/page.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import { Summary, TurboSummary } from '@/components/summary';
+import { Separator } from '@repo/ui/components/separator';
 import { useEffect, useState } from 'react';
+import { UploadForm } from '../page.client';
 
 export default function LocalPage() {
-  const [data, setData] = useState<{ data: TurboSummary } | undefined>();
+  const [data, setData] = useState<{ id: string; data?: TurboSummary } | undefined>();
   useEffect(() => {
     const id = window.location.hash.slice(1);
-    const data = JSON.parse(
-      localStorage.getItem(`turbo-summary/${id}`) ?? JSON.stringify({ id, data: null }),
-    );
-    setData(data);
+    const data = JSON.parse(localStorage.getItem(`turbo-summary/${id}`) ?? '{}');
+    setData({ id, ...data });
   }, []);
   const loading = !data;
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="container mx-auto flex flex-1 items-center justify-center p-4">
+        Loading...
+      </div>
+    );
   }
 
   if (!data.data) {
-    return <div>No data found for ID: {data.id}</div>;
+    return (
+      <div className="container mx-auto flex flex-1 flex-col items-center justify-center gap-4 p-4">
+        <p className="text-lg">No data found for ID: {data.id}</p>
+        <p className="text-muted-foreground text-sm">Try to upload your summary again</p>
+
+        <UploadForm />
+      </div>
+    );
   }
 
   return (
